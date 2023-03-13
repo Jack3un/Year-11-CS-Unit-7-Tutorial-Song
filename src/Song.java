f5c62fcdc22391703d428a31a6899d7ded4ae58b
Jack3un <jacks2025@student.cis.edu.hk>
2023-03-13 21:22:44 +0800

    completed

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
-public class Song {
+import java.util.ArrayList;
+import java.util.HashSet;
 
-    private String
+public class Song
+{
+    private String Artist;
+    private String Title;
+    private HashSet<String> a;
+
+    public Song(String title, String artist)
+    {
+        this.Title = title;
+        this.Artist = artist;
+        a = new HashSet<String>();
+    }
+    public String getTitle()
+    {
+        return Title;
+    }
+    public String getArtist()
+    {
+        return Artist;
+    }
+
+    public int howMany(ArrayList<String> Name)
+    {
+        for (int i = 0; i < Name.size(); i++)
+        {
+            Name.set(i, Name.get(i).toLowerCase());
+        }
+        int previousSize = a.size();
+        a.addAll(Name);
+        return (a.size() - previousSize);
+    }
 }
